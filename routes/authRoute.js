4f6fc9643feec4cb82d3dc46c6af40fd3eaf2f8d
Sunayan Das <sunayan.das040@gmail.com>
2023-04-08 00:39:58 +0530

    get orders and update order status

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -21,17 +21,21 @@ const {
   emptyCart,
   applyCoupon,
   createOrder,
+  getOrders,
+  updateOrderStatus,
 } = require("../controllers/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 router.post("/register", createUser);
 router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/reset-password/:token", resetPassword);
+
 router.put("/password", authMiddleware, updatePassword);
 router.post("/login", loginUserCtrl);
 router.post("/admin-login", loginAdmin);
 router.post("/cart", authMiddleware, userCart);
 router.get("/all-users", getAllUsers);
+router.get("/get-orders", authMiddleware, getOrders);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
 router.get("/wishlist", authMiddleware, getWishList);
@@ -42,6 +46,12 @@ router.post("/cart/cash-order", authMiddleware, createOrder);
 router.get("/:id", authMiddleware, getaUser);
 router.delete("/empty-cart", authMiddleware, emptyCart);
 router.delete("/:id", deleteaUser);
+router.put(
+  "/order/update-order/:id",
+  authMiddleware,
+  isAdmin,
+  updateOrderStatus
+);
 router.put("/edit-user", authMiddleware, updateaUser);
 router.put("/save-address", authMiddleware, saveAddress);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
